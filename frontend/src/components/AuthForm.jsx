a24810df6aa80d517f81b3964ca9ec7e924c0b27
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-08-19 10:46:16 +0300

    feat: added displaying validation errors

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,16 +1,35 @@
-import { Form, Link, useSearchParams } from 'react-router';
+import {
+	Form,
+	Link,
+	useActionData,
+	useNavigation,
+	useSearchParams,
+} from 'react-router';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
 	const [searchParams] = useSearchParams();
+	const data = useActionData();
+	const navigation = useNavigation();
+
 	const mode = searchParams.get('mode');
 	const isLogin = !mode || mode === 'login';
 	const otherMode = isLogin ? 'signup' : 'login';
 
+	const isSubmitting = navigation.state === 'submitting';
+
 	return (
 		<Form method="post" className={classes.form}>
 			<h1>{isLogin ? 'Log in' : 'Sign up'}</h1>
+			{data?.message && <p>{data.message}</p>}
+			{data?.errors && (
+				<ul>
+					{Object.entries(data.errors).map(([key, err]) => (
+						<li key={key}>{err}</li>
+					))}
+				</ul>
+			)}
 			<p>
 				<label htmlFor="email">Email</label>
 				<input id="email" type="email" name="email" required />
@@ -25,7 +44,9 @@ function AuthForm() {
 						? 'Create new account'
 						: 'Log in with existing account'}
 				</Link>
-				<button>Submit</button>
+				<button disabled={isSubmitting}>
+					{isSubmitting ? 'Submitting...' : 'Submit'}
+				</button>
 			</div>
 		</Form>
 	);
